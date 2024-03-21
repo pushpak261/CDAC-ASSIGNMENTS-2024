7aed84f6181b843b4a9a42da2ee459d87cb90403
Pushpak Pandharpatte <66894294+pushpak261@users.noreply.github.com>
2024-03-21 08:09:52 +0530

    Add files via upload

---
+++ Assign 3/WideningConversion.java
@@ -0,0 +1,18 @@
+public class WideningConversionDemo {
+    public static void main(String[] args) {
+        // Initialize an int variable
+        int intValue = 42;
+
+        // Perform widening conversions and print the results
+        double doubleValue = intValue; // Widening conversion to double
+        float floatValue = intValue;  // Widening conversion to float
+        boolean booleanValue = (intValue != 0); // Widening conversion to boolean (non-zero int is true)
+        String stringValue = String.valueOf(intValue); // Widening conversion to String
+
+        // Print the results
+        System.out.println("Widening conversion from int to double: " + doubleValue);
+        System.out.println("Widening conversion from int to float: " + floatValue);
+        System.out.println("Widening conversion from int to boolean: " + booleanValue);
+        System.out.println("Widening conversion from int to String: " + stringValue);
+    }
+}
---
+++ Assign 3/widening.java
@@ -0,0 +1,9 @@
+public class widening {
+    public static void main(String[] args) {
+        int intValue = 10;
+        double doubleValue = intValue; 
+
+        System.out.println("Original int value: " + intValue);
+        System.out.println("Widened double value: " + doubleValue);
+    }
+}
