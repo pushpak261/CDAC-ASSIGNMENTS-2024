172efab51bd0e0e16ea94ef6189476f2159d0c22
Pushpak Pandharpatte <66894294+pushpak261@users.noreply.github.com>
2024-03-21 08:20:48 +0530

    Update narrowing.java

--- Assign 3/narrowing.java
+++ Assign 3/narrowing.java
@@ -1,8 +1,8 @@
-class Narrowing {
+class narrowing {
     public static void main(String[] args) {
 
         double d = 5.50;
-        int i = (int) d;
+        int i = (int) d;                        // Performs a narrowing conversion from double to int, explicitly casting d to int
 
         System.out.println(i);
     }
