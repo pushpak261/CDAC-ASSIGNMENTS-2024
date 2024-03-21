659b7ab49a9a50894974f3b128c3f826ad6fcd83
Pushpak Pandharpatte <66894294+pushpak261@users.noreply.github.com>
2024-03-21 08:53:26 +0530

    Update NarrowingConversion.java

--- Assign 3/NarrowingConversion.java
+++ Assign 3/NarrowingConversion.java
@@ -5,11 +5,13 @@ public class MyClass {
         long l = 15;
         float f = 15.00f;
 
-        double dResult = i + d; // Widening conversion: int i is promoted to double
-        long lResult = i + l;   // Widening conversion: int i is promoted to long
-        float fResult = i + f;  // Widening conversion: int i is promoted to float
+        double dResult = i + d;                       //This line performs an addition operation between i and d. Since d is a double and i is an integer, i is implicitly converted (widened) to a double before the addition.
 
-        System.out.println("double : " + dResult);
+        
+        long lResult = i + l;                          // Similar to the previous line, here i is added to l. Since l is a long and i is an integer, i is implicitly converted (widened) to a long before the addition.
+        float fResult = i + f;                        // Here, i is added to f. Since f is a float and i is an integer, i is implicitly converted (widened) to a float before the addition.   
+
+        System.out.println("double : " + dResult);                      //Finally, the results of the addition operations are printed to the console.
         System.out.println("long : " + lResult);
         System.out.println("float : " + fResult);
     }
