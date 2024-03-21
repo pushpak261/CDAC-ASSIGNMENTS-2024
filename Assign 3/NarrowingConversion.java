491ec8d1ea24e60b29204788697b97d3cfb75706
Pushpak Pandharpatte <66894294+pushpak261@users.noreply.github.com>
2024-03-21 08:24:22 +0530

    Update NarrowingConversion.java

--- Assign 3/NarrowingConversion.java
+++ Assign 3/NarrowingConversion.java
@@ -1,16 +1,16 @@
-class Calculator {
+public class MyClass {
     public static void main(String[] args) {
-        int i=5;
-        double d=10.00;
-        long l=15;
-        float f =15.00f;
+        int i = 5;
+        double d = 10.00;
+        long l = 15;
+        float f = 15.00f;
 
-        double dResult = i+d;
-        long lResult = i+l;
-        float fResult= i+f;
+        double dResult = i + d; // Widening conversion: int i is promoted to double
+        long lResult = i + l;   // Widening conversion: int i is promoted to long
+        float fResult = i + f;  // Widening conversion: int i is promoted to float
 
-        System.out.println("double : "+dResult);
-        System.out.println("long : "+lResult);
-        System.out.println("float : "+fResult);
+        System.out.println("double : " + dResult);
+        System.out.println("long : " + lResult);
+        System.out.println("float : " + fResult);
     }
 }
