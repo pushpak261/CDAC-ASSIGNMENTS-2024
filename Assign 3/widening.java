dcc71820e65f37d250a2ea60aaf4a73d8366ff02
Pushpak Pandharpatte <66894294+pushpak261@users.noreply.github.com>
2024-03-21 08:19:48 +0530

    Update widening.java

--- Assign 3/widening.java
+++ Assign 3/widening.java
@@ -1,9 +1,10 @@
 public class widening {
     public static void main(String[] args) {
         int intValue = 10;
-        double doubleValue = intValue; 
-
+        double doubleValue = intValue;                                //// Performs a widening conversion from int to double
+        
         System.out.println("Original int value: " + intValue);
         System.out.println("Widened double value: " + doubleValue);
     }
 }
+
